bf596dee9fbdd56cd5428f356d027a38aa0ef4b4
Ras Brits <rasbrits@yahoo.com>
2021-01-06 22:51:39 +0200

    Added method

--- src/methods.ts
+++ src/methods.ts
@@ -75,3 +75,29 @@ export function applyCellDimensionOffsets(
 
   return { x: nX, y: nY };
 }
+
+export function makeHash( str: String ) {
+  if (str.length % 32 > 0) str += Array(33 - str.length % 32).join("z");
+  var i, j, k, a; i = j = k = a = 0;
+  var hash = '', bytes = [], dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','1','2','3','4','5','6','7','8','9'];
+  for (i = 0; i < str.length; i++ ) {
+      var ch = str.charCodeAt(i);
+      bytes[j++] = (ch < 127) ? ch & 0xFF : 127;
+  }
+  var chunk_len = Math.ceil(bytes.length / 32);   
+  for (i=0; i<bytes.length; i++) {
+      j += bytes[i];
+      k++;
+      if ((k === chunk_len) || (i === bytes.length-1)) {
+          a = Math.floor( j / k );
+          if (a < 32)
+              hash += '0';
+          else if (a > 126)
+              hash += 'z';
+          else
+              hash += dict[  Math.floor( (a-32) / 2.76) ];
+          j = k = 0;
+      }
+  }
+  return hash;
+}
